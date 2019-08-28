cc0d2014fc493bd88389f22c8a6d1a02fe1fd975
Joseph Minadeo <hizzlepizzle@hotmail.com>
2019-08-28 16:49:57 -0700

    Initial Commit

---
+++ datacorrupter/README.md
@@ -0,0 +1,11 @@
+# Patatap
+
+Patatap is an animation and sound kit. Create melodies with the touch of your fingers.
+
+## How to use:
+
+Pressing any key from A to Z will produce a sound. An animated circle will also randomly appear on the canvas.
+
+## Tech:
+
+HTML, CSS, JavaScript, Paper.js, Howler.js
---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+  <title>EarthQuaker Devices Randomizer and Samplepacks by PatternBased</title>
+  
+  
+  	<style>
+	  table.center {
+			width: 100%;
+			height: 100%;
+			margin: 0;
+			padding: 0;
+			position: absolute;
+			margin-left: 5%; 
+			margin-right: 5%;
+			}
+	   
+	   html {
+			background-color: #000000; 
+			}
+			
+	</style>
+  
+</head>
+
+<body>
+
+
+<?php
+	// 1. Create a database connection
+	$dbhost = "localhost";
+	$dbuser = "wordpress-user";
+	$dbpass = "Doody!ppMARIA";
+	$dbname = "patternbased-db";
+	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+	// Test if connection occured
+	if(mysqli_connect_errno()) {
+	  die("Database connection failed: " .
+	  	mysqli_connect_error() .
+  	  	" (" . mysqli_connect_errno() . ")"
+	  );
+	}
+?>
+
+<?php
+	// 2. Build the MYSQL Query to randomly select three EQDevices and put the associated data into an array
+
+	$query  = "SELECT * ";
+	$query .= "FROM eqd ";
+	$query .= "WHERE IsActive = 1 ";
+	$query .= "order by RAND() ";
+	$query .= "LIMIT 3";
+
+	// uncomment the following echo statement to see a printout of the database query
+	// echo "Database Query: " . $query . "<br />";
+
+	$result = mysqli_query($connection, $query);
+	// Test if there was a query error
+	if (!$result) {
+		die("Database query failed.");
+	}
+?>
+<table class="center">
+<tr>
+<?php
+	// 3. Loop returned data into an array
+		while($subject = mysqli_fetch_assoc($result)) {
+	// output data from each row
+?>
+
+<td>
+<?php echo "<a href=\"" . $subject["SamplePackURL"] . "\">" . "<img src=\"images/" . $subject["DeviceImage"] . "\" style=\"height:300px;\">" . "</a>" ; ?>
+</td>
+
+<?php
+	}
+?>
+</tr>
+</table>
+
+<?php
+	// 4. Release returned data
+	mysqli_free_result($result);
+?>
+
+<?php
+  // 5. Close database connection
+  mysqli_close($connection);
+?>
+
+
+</body>
+
+</html>
