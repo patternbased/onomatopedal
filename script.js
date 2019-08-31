788a9e5be3278110439fc0e104f82b4a249a7ee3
Siori Kitajima <siorikitajima@sioris-mbp.lan>
2019-08-30 23:51:16 -0700

    Added Fullscreen mode in Pedal page

--- script.js
+++ script.js
@@ -26,3 +26,39 @@ $('.actionBtn').hover(function () {
     'transition-duration':'50ms'
   })
 });
+
+$('#fullscreenMode').click(function () { 
+  $('#pedal').fadeOut(500);
+  $('footer').css({
+    'position': 'fixed',
+  });
+  $('#showAll').css({
+    'visibility': 'visible'
+  });
+});
+
+$('#showAll').hover(function () {
+  $(this).css({
+    'width': '220px',
+    'background': '#dfc669',
+    'transition-duration':'100ms'
+  }),
+  $('span', this).css('display','inline')
+  }, function () {
+    $('span', this).css('display','none')
+    $(this).css({
+      'width': '50px',
+      'background': '#ffffff',
+      'transition-duration':'50ms'
+    });
+  }
+);
+
+$('#showAll').click(function () { 
+  $('#pedal').fadeIn(300);
+  $('footer').css({
+    'position': 'static',
+  });
+  $('#showAll').css({
+    'visibility': 'hidden'});
+  });
