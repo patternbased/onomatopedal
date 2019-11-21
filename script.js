18f96ace20228743960408a23826df447b0c14d6
siorikitajima <siorikitajima@sioris-imac.lan>
2019-11-21 00:03:58 -0800

    Bug fix 6

--- script.js
+++ script.js
@@ -58,7 +58,6 @@ $(pedalBtn).click(function() {
   var modalCache = localStorage.getItem('modal');
   localStorage.clear();
   localStorage.setItem('modal',modalCache);
-  
   $('.loadingSpinner').css('display','inline-block');
   $('#modalBG').css('display','block');
 
